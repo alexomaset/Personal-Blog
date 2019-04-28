b973a747f8f064133ad12460752296f185d34b0b
alexomaset <alexomaset711@gmail.com>
2019-04-29 01:33:03 +0300

    added Forms

--- personalblog.py
+++ personalblog.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@approute("/index")
+def index():
+    return render_template
+
+if __name__ = '__main__':
+    app.run(debug=True)
