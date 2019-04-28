2e64ecdf6de54e9f52dfe594a9f8b06f93980b1d
alexomaset <alexomaset711@gmail.com>
2019-04-29 01:39:27 +0300

    modified config file

--- forms.py
+++ forms.py
@@ -7,10 +7,10 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('password' validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField("Sign Up")
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm): 
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('password' validators=[DataRequired()])
     remember = BooleanField('Remember me')
-    submit = SubmitField("Login ")
+    submit = SubmitField('Login')
--- personalblog.py
+++ personalblog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
+ 
+app.config['SECRET_KEY'] = 'ekiomz1234'
 
 @app.route("/")
 @approute("/index")
