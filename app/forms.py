bb989627368f8203549da4e7ef3805a411dca0e1
alexomaset <alexomaset711@gmail.com>
2019-04-29 15:43:03 +0300

    adding Blueprints

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtfforms.validators import DataRequired, Length, Email, EqualTo
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequire(), Length(min=2, max=20)])
@@ -8,6 +9,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password' validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
 
 class LoginForm(FlaskForm): 
     email = StringField('Email', validators=[DataRequired(), Email()])
