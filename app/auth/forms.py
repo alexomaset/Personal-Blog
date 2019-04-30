9aaffd7692205ddfe0acf71ee3f9b99a69c3b7fb
alexomaset <alexomaset711@gmail.com>
2019-04-30 08:07:59 +0300

    added validation to forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtfforms.validators import DataRequired, Length, Email, EqualTo
-from app.models import User
+from ..models import User
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequire(), Length(min=2, max=20)])
@@ -15,6 +16,7 @@ class RegistrationForm(FlaskForm):
     
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
+
 
 class LoginForm(FlaskForm): 
     email = StringField('Email', validators=[DataRequired(), Email()])
